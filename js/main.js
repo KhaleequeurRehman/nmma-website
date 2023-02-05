1cd40a5fb3e40dc1283e0b68cb82973fcb9f410e
khaleeque <kmem98@gmail.com>
2023-02-06 03:02:45 +0500

    code updated with special events page

--- js/main.js
+++ js/main.js
@@ -1,20 +1,28 @@
-$(document).ready(function(){
-    $('.owl-carousel').owlCarousel({
-        loop:true,
-        margin:15,
-        responsiveClass:true,
-        nav:false,
-        dots:true,
-        responsive:{
-            0:{
-                items:1,
-            },
-            600:{
-                items:3,
-            },
-            1000:{
-                items:4,
-            }
-        }
-    })
-  });
+$(function() {
+    // $("h1").lettering();
+    // $(".eventcurveheading").lettering();
+    $(".specialeventscurveheading").lettering();
+});
+
+// $(document).ready(function(){
+//     $('.owl-carousel').owlCarousel({
+//         loop:true,
+//         margin:15,
+//         responsiveClass:true,
+//         nav:false,
+//         dots:true,
+//         responsive:{
+//             0:{
+//                 items:1,
+//             },
+//             600:{
+//                 items:3,
+//             },
+//             1000:{
+//                 items:4,
+//             }
+//         }
+//     })
+//   });
+
+
